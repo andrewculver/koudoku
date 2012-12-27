837c9df80e74955cfb7ea75710d00eaa35457ad1
Andrew Culver <andrew.culver@gmail.com>
2012-12-27 17:29:22 -0500

    Adding configuration.

--- lib/koudoku.rb
+++ lib/koudoku.rb
@@ -2,4 +2,12 @@ require "koudoku/version"
 require "koudoku/subscription"
 
 module Koudoku
+
+  mattr_accessor :webhooks_api_key
+  @@webhooks_api_key = nil
+  
+  def self.setup
+    yield self
+  end
+  
 end
