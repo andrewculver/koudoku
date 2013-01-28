a08a7a108f36ebbc06101509d8714031fb2b1c1e
Andi Bade <andi@galaxycats.com>
2013-01-28 11:07:51 +0100

    use StripeInterface everywhere

--- lib/koudoku/stripe_interface.rb
+++ lib/koudoku/stripe_interface.rb
@@ -9,4 +9,28 @@ module Koudoku
       raise LoadError, %Q{Koudoku only works with either the 'stripe' gem or the 'paymill' gem. So please add "gem 'stripe'" or "gem 'paymill'" to your Gemfile _before_ loading koudoku.}
     end
 
+  class <<StripeInterface
+    def stripe?
+      stripe_library_name == :stripe
+    end
+
+    def paymill?
+      stripe_library_name == :paymill
+    end
+
+    def stripe_library_name
+      self.name.underscore.to_sym
+    end
+
+    def error_class
+      case
+      when self.stripe?
+        Stripe::StripeError
+      when self.paymill?
+        Paymill::PaymillError
+      end
+    end
+
+  end
+
 end
