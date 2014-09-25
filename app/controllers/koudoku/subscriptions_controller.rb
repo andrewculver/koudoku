0e07d48e95b9b29eb464e0b86db57122786e6577
Christoph Engelhardt <christoph@it-engelhardt.de>
2014-09-25 11:56:52 +0200

    -bugfix: Fixed a bug that caused redirects to fail if subscription owner was not :user

--- app/controllers/koudoku/subscriptions_controller.rb
+++ app/controllers/koudoku/subscriptions_controller.rb
@@ -61,7 +61,8 @@ module Koudoku
     end
 
     def redirect_to_sign_up
-      session["#{Koudoku.subscriptions_owned_by.to_s}_return_to"] = new_subscription_path(plan: params[:plan])
+      # "user_return_to" is a name used by Devise - no use in changing it based on subscription owner
+      session["user_return_to"] = new_subscription_path(plan: params[:plan])
       redirect_to new_registration_path(Koudoku.subscriptions_owned_by.to_s)
     end
 
