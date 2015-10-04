bac6daa76c43bc90a508db4cd1390d66a0e43b14
Christoph Engelhardt <christoph@it-engelhardt.de>
2015-10-04 07:48:04 +0200

    -bugfix: Fixing messed up routes in 1.2.0

--- app/controllers/koudoku/application_controller.rb
+++ app/controllers/koudoku/application_controller.rb
@@ -3,8 +3,5 @@ module Koudoku
     layout Koudoku.layout
     helper :application
     
-    # stolen from http://stackoverflow.com/questions/9232175/access-main-app-helpers-when-overridings-a-rails-engine-view-layout#comment44856124_19453140
-    # to solve the problem of having to reference "main_app.$some_model_path" in layouts
-    helper Rails.application.routes.url_helpers 
   end
 end
