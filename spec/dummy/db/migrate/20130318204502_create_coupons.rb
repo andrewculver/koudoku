d9c441e33714a35c540b8af35d173acc67019558
William Porter <dawilster143@gmail.com>
2016-01-19 14:11:20 +1100

    Add all the required Coupon attributes to mirror Stripe's coupons

--- spec/dummy/db/migrate/20130318204502_create_coupons.rb
+++ spec/dummy/db/migrate/20130318204502_create_coupons.rb
@@ -3,6 +3,13 @@ class CreateCoupons < ActiveRecord::Migration
     create_table :coupons do |t|
       t.string :code
       t.string :free_trial_length
+      t.integer :coupons, :interval, :integer
+      t.float :coupons, :amount_off, :float
+      t.integer :coupons, :percentage_off, :integer
+      t.datetime :coupons, :redeem_by, :datetime
+      t.integer :coupons, :max_redemptions, :integer
+      t.integer :coupons, :duration, :integer
+      t.integer :coupons, :duration_in_months, :integer
 
       t.timestamps
     end
