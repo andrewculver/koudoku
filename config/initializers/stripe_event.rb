cae1da1d06cb50720fce402123a061a7f58fe663
Andrew Culver <andrew.culver@gmail.com>
2019-04-23 23:47:06 -0700

    Have StripeEvent ignore Stripe Connect webhooks.

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -1,25 +1,28 @@
 StripeEvent.configure do |events|
   events.subscribe 'charge.failed' do |event|
+    return nil if event.try(:account)
     stripe_id = event.data.object['customer']
-      
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
     subscription.charge_failed
   end
-  
+
   events.subscribe 'invoice.payment_succeeded' do |event|
+    return nil if event.try(:account)
     stripe_id = event.data.object['customer']
     amount = event.data.object['total'].to_f / 100.0
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
     subscription.payment_succeeded(amount)
   end
-  
+
   events.subscribe 'charge.dispute.created' do |event|
+    return nil if event.try(:account)
     stripe_id = event.data.object['customer']
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
     subscription.charge_disputed
   end
-  
+
   events.subscribe 'customer.subscription.deleted' do |event|
+    return nil if event.try(:account)
     stripe_id = event.data.object['customer']
     subscription = ::Subscription.find_by_stripe_id(stripe_id)
     subscription.subscription_owner.try(:cancel)
